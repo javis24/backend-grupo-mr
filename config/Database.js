07df07ef439601f6f383f0fa954f6b771cda87b6
Javier Mtz <luisjaviermtzb@gmail.com>
2024-08-18 15:37:23 -0600

    new config commit

--- config/Database.js
+++ config/Database.js
@@ -1,8 +1,17 @@
-import {Sequelize} from "sequelize";
+import { Sequelize } from "sequelize";
+import dotenv from 'dotenv';
 
-const db = new Sequelize('auth_db_grm', 'root', '', {
-    host: "localhost",
-    dialect: "mysql"
-});
+dotenv.config();
+
+const db = new Sequelize(
+    process.env.MYSQLDATABASE || process.env.DB_NAME,
+    process.env.MYSQLUSER || process.env.DB_USER,
+    process.env.MYSQLPASSWORD || process.env.DB_PASSWORD,
+    {
+        host: process.env.MYSQLHOST || process.env.DB_HOST,
+        dialect: process.env.DB_DIALECT || 'mysql',
+        port: process.env.MYSQLPORT || 3306,  // Usamos el puerto de MySQL
+    }
+);
 
 export default db;
