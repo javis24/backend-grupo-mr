3c743d9ea1964f82052bd3be1c4132d38d6b068d
Javier Mtz <luisjaviermtzb@gmail.com>
2024-08-18 15:33:56 -0600

    sin env commit

--- config/Database.js
+++ config/Database.js
@@ -1,17 +1,8 @@
-import { Sequelize } from "sequelize";
-import dotenv from 'dotenv';
+import {Sequelize} from "sequelize";
 
-dotenv.config();
-
-const db = new Sequelize(
-    process.env.DB_NAME,
-    process.env.DB_USER,
-    process.env.DB_PASSWORD,
-    {
-        host: process.env.DB_HOST,
-        dialect: process.env.DB_DIALECT,
-        port: process.env.APP_PORT,  // Asegúrate de incluir el puerto si es necesario
-    }
-);
+const db = new Sequelize('auth_db_grm', 'root', '', {
+    host: "localhost",
+    dialect: "mysql"
+});
 
 export default db;
