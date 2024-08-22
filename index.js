fa2f9366b00aced4c55ff3cdb2a8a01059f642c6
Javier Mtz <luisjaviermtzb@gmail.com>
2024-08-21 19:58:39 -0600

    update cors index commit

--- index.js
+++ index.js
@@ -3,15 +3,15 @@ import cors from "cors";
 import session from "express-session";
 import dotenv from "dotenv";
 import db from "./config/Database.js";
-import  SequelizeStore  from "connect-session-sequelize";
-import UserRoute  from "./routes/UserRoute.js";
+import SequelizeStore from "connect-session-sequelize";
+import UserRoute from "./routes/UserRoute.js";
 import ProductRoute from "./routes/ProductRoute.js";
 import AuthRoute from "./routes/AuthRoute.js";
 import ClientRoute from "./routes/ClientRoute.js";
 import CompanyRoute from "./routes/CompanyRoute.js";
 import BusinessUnitRoute from "./routes/BusinessUnitRoute.js";
-import TrafficLightRoute from "./routes/TrafficLightRoute.js"; 
-import EventRoute from "./routes/EventRoute.js"
+import TrafficLightRoute from "./routes/TrafficLightRoute.js";
+import EventRoute from "./routes/EventRoute.js";
 
 dotenv.config();
 
@@ -23,10 +23,9 @@ const store = new sessionStore({
     db: db
 });
 
- (async()=>{
-     await db.sync();
- })();
-
+(async () => {
+    await db.sync();
+})();
 
 app.use(session({
     secret: process.env.SESS_SECRET,
@@ -38,24 +37,24 @@ app.use(session({
     }
 }));
 
-
+// Ajuste de CORS
 app.use(cors({
     credentials: true,
-    origin: 'https://backend-grupo-mr-production-2c03.up.railway.app'
+    origin: 'https://grupomrlaguna.com' 
 }));
 
 app.use(express.json());
 app.use(UserRoute);
 app.use(ProductRoute);
-app.use(ClientRoute)
+app.use(ClientRoute);
 app.use(AuthRoute);
 app.use(CompanyRoute);
 app.use(BusinessUnitRoute);
 app.use(TrafficLightRoute);
 app.use(EventRoute);
 
-  store.sync();
+store.sync();
 
-  app.listen(process.env.PORT || 5000, () => {
+app.listen(process.env.PORT || 5000, () => {
     console.log(`Servidor corriendo en el puerto ${process.env.PORT || 5000}...`);
 });
